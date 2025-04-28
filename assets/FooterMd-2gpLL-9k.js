9117654e12459a4a5eeac582f66cb63459f4fff7
mateus-lopes <50590861+mateus-lopes@users.noreply.github.com>
2025-04-28 23:05:47 +0000

    Deploying to gh-pages from @ mateus-lopes/portifolio@713b6926b3171328d92951f00eb5c4134c252313 🚀

--- assets/FooterMd-2gpLL-9k.js
+++
@@ -1,1 +0,0 @@
-import{C as o,S as t,F as a}from"./FooterSignature-Bmsuli3Z.js";import{d as c,c as r,o as n,a as s,h as e}from"./index-WNjqTnak.js";const _={id:"contact"},f=c({__name:"FooterMd",setup(i){return(p,d)=>(n(),r("section",_,[s("footer",null,[e(o),e(t),e(a)])]))}});export{f as default};
