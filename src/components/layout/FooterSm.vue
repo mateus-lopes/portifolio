222b5634dfbbcad25102a175cdc7110fbd8e56a2
mateus-lopes <mateusalbano22@gmail.com>
2025-04-28 12:25:50 -0300

    FEAT: Implement responsive header and footer components

---
+++ src/components/layout/FooterSm.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ContactSection from './footer/ContactSection.vue'
+import ScrollUp from './footer/ScrollUp.vue'
+import FooterSignature from './footer/FooterSignature.vue'
+</script>
+
+<template>
+  <section id="contact">
+    <footer>
+      <h1>Footer- SM</h1>
+      <ScrollUp />
+      <FooterSignature />
+    </footer>
+  </section>
+</template>
